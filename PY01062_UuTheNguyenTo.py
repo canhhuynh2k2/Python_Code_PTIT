aa334dc8751f4680077ce48c4562e1c6e3d7a198
Nguyễn Cảnh Huỳnh <105232878+canhhuynh2k2@users.noreply.github.com>
2022-08-18 21:58:04 +0700

    ƯU THẾ NGUYÊN TỐ

---
+++ PY01062_UuTheNguyenTo.py
@@ -0,0 +1,17 @@
+from math import *
+t = int(input())
+def isPrime(n):
+    if n < 2: return false
+    for x in range(2, int(sqrt(n)) + 1):
+        if n % x == 0: return False
+    return True
+while t > 0:
+    n = input()
+    Prime = 0
+    notPrime = 0
+    for x in n:
+        if x == '2' or x == '3' or x == '5' or x == '7': Prime += 1
+        else: notPrime += 1
+    if Prime > notPrime and isPrime(len(n)): print("YES")
+    else: print("NO")
+    t -= 1
