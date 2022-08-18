818f6e870c0f8872f169c8b424e3df2057897be3
Nguyễn Cảnh Huỳnh <105232878+canhhuynh2k2@users.noreply.github.com>
2022-08-18 21:53:11 +0700

    TỔNG CHỮ SỐ THUÂN NGHỊCH

---
+++ PY01051_TongChuSoThuanNghich.py
@@ -0,0 +1,10 @@
+t = int(input())
+d = {'0' : 0, '1' : 1, '2' : 2, '3' : 3, '4' : 4, '5' : 5, '6' : 6, '7' : 7, '8' : 8, '9' : 9}
+while t > 0:
+    s = input()
+    sum = 0
+    for x in s:
+       sum += d[x]
+    if sum > 9 and str(sum) == str(sum)[-1::-1]: print("YES")
+    else: print("NO")
+    t -= 1
